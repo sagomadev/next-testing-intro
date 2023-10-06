e31e7d10a4551344a593443b1c22528bd20bb860
Samuel Gómez <sagomadev@gmail.com>
2023-10-06 19:04:16 +0200

    Add test example and change version of dependency

--- jest.config.mjs
+++ jest.config.mjs
@@ -8,8 +8,8 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const config = {
-  // Add more setup options before each test is run
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  // // Add more setup options before each test is run
+  // setupFilesAfterEnv: ["<rootDir>/jest.setup.mjs"],
   testEnvironment: "jest-environment-jsdom",
   preset: "ts-jest",
 };
